8ff36ec8bb68af0550d3c97e54916e317d393d8f
ashutosh1ibm <ashutoshk5029@gmail.com>
2020-07-23 06:51:20 +0530

    first Ant commit

---
+++ src/main/java/com/ibm/test/MathematicOperation.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ibm.test;
+
+/**
+ * @author AshutoshKumar
+ *
+ */
+public class MathematicOperation {
+
+	public int add(int a, int b) {
+		int res = a + b;
+		return res;
+	}
+}
---
+++ src/main/java/com/ibm/test/Test.java
@@ -0,0 +1,16 @@
+package com.ibm.test;
+
+/**
+ * @author AshutoshKumar
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int res = new MathematicOperation().add(4, 5);
+		
+		System.out.println("res : "+res);
+	}
+
+}
